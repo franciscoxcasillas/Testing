29b2765e2913d1effc745f287d20530d11b7e610
Francisco Casillas <chiccasillas@gmail.com>
2025-02-04 10:38:01 -0600

    Initial Commit

---
+++ Testing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Testing
+//
+//  Created by Francisco Jean on 04/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Francisco Jean on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
