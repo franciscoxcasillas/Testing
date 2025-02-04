2f1195c3f9c4964125a59e9ef923f23963079c75
Francisco Casillas <chiccasillas@gmail.com>
2025-02-04 10:41:55 -0600

    Try ZStack layouts

--- Testing/ContentView.swift
+++ Testing/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.red
+            Circle()
+                .fill(Color.yellow)
+                .frame(width: 200, height: 200)
+            Text("Layered View")
+                .foregroundColor(.white)
         }
-        .padding()
     }
 }
 
